3075019cd68618e97775e875a89af9304adef492
mehdinathani <mehdinathani@gmail.com>
2025-01-01 20:37:50 +0500

    first commit

---
+++ src/sanity/products.ts
@@ -0,0 +1,39 @@
+import { defineField, defineType } from "sanity";
+
+export const productType = defineType({
+    name: "produt",
+    type: 'document',
+    title: "New Product",
+    fields: [
+        defineField({
+            name: "productName",
+            title: "Product Name",
+            type: "string"
+
+        }),
+        defineField({
+            name: "productPrice",
+            type: "number",
+            title: "Price"
+        }),
+        defineField({
+            name: "productImage",
+            type: "image",
+            title: "Image"
+        })
+    ]
+})
+
+
+export const postType = defineType({
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            title: ' Post Title',
+            type: 'string',
+        })
+    ]
+});
